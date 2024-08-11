17ecefc3f28a9a9965fc2e211d262c0cee6edeb8
rink <77056883+rin-k-dev@users.noreply.github.com>
2024-08-11 14:57:30 +0900

    #introductionの実装

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,16 @@
         </p>
       </div>
     </section>
+    <section id="introduction">
+      <div class="introduction__container">
+        <h2 class="introduction__title noto-sans-jp">Rin K</h2>
+        <p class="introduction__text noto-sans-jp">
+          <span>千葉県在住のフリーランスWebコーダーです。</span>
+          <span>ポートフォリオをご覧いただきありがとうございます。</span>
+          <span>Web制作のお仕事承ります。</span>
+        </p>
+      </div>
+    </section>
   </div>
 
 
