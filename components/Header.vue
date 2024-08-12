661a903a55514693199d067015d89e82adab025e
rink <77056883+rin-k-dev@users.noreply.github.com>
2024-08-12 20:11:26 +0900

    制作実績ページの追加

--- components/Header.vue
+++ components/Header.vue
@@ -12,10 +12,10 @@
       </div>
       <nav class="header__nav" :class="{ open: isMenuOpen }">
         <ul>
-          <li><a class="header__link-service noto-sans-jp" href="#service" @click="closeMenu">Service</a></li>
-          <li><a class="header__link-works noto-sans-jp" href="#works" @click="closeMenu">Works</a></li>
-          <li><a class="header__link-about noto-sans-jp" href="#about" @click="closeMenu">About</a></li>
-          <li><a class="header__link-contact noto-sans-jp" href="#contact" @click="closeMenu">Contact</a></li>
+          <li><a class="header__link-service noto-sans-jp" href="/#service" @click="closeMenu">Service</a></li>
+          <li><a class="header__link-works noto-sans-jp" href="/#works" @click="closeMenu">Works</a></li>
+          <li><a class="header__link-about noto-sans-jp" href="/#about" @click="closeMenu">About</a></li>
+          <li><a class="header__link-contact noto-sans-jp" href="/#contact" @click="closeMenu">Contact</a></li>
         </ul>
       </nav>
     </div>
---
+++ pages/works.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Header></Header>
+    <main id="works">
+
+    </main>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from '~/components/Header.vue';
+import Footer from '~/components/Footer.vue';
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
+
+<style scoped>
+/* ページ固有のスタイル */
+
+</style>
