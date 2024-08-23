6c2a7c9df49ab6d0086c35082cccf7de079abe5e
rink <77056883+rin-k-dev@users.noreply.github.com>
2024-08-23 18:24:06 +0900

    すぐ食べの実装

--- pages/sugutabe.vue
+++ pages/sugutabe.vue
@@ -53,6 +53,7 @@
               <img class="sp_only work-page__work-detail-thumbnail-sp" src="~/assets/image/work/sugutabe_thumb_sp_sp.png" alt="">
               <img class="pc_only work-page__work-detail-thumbnail-sp" src="~/assets/image/work/sugutabe_thumb_sp_pc.png" alt="">
             </div>
+            <a class="work-page__link-button" href="https://webcreatorrin.com/portfolio/sugutabe">サイトを見る</a>
           </div>
         </div>
       </section>
